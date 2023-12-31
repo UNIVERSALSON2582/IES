785303432a302a459c15445797442047dd6f447d
UNIVERSALSON2582 <dnegi2582@gmail.com>
2024-01-01 01:36:23 +0530

    new

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,37 +1,13 @@
-import React, { useState } from 'react';
+import React, { Children } from 'react';
 
-function Contact() {
-  const [isChatboxOpen, setChatboxOpen] = useState(false);
 
-  const chatboxToggleHandler = () => {
-    setChatboxOpen(!isChatboxOpen);
-  };
+const Button = (props,{onClick}) =>{
 
-  return (
-    <div>
-      <div className="flex items-center justify-center p-12">
-        <div className="w-full">
-          <div className="formbold-form-wrapper mx-auto hidden w-full max-w-[550px] rounded-lg border border-[#e0e0e0] bg-white">
-            <div className="flex items-center justify-between rounded-t-lg bg-[#6A64F1] py-4 px-9">
-              <h3 className="text-xl font-bold text-white">Let's chat? - Online</h3>
-              <button onClick="chatboxToggleHandler()" className="text-white">
-                {/* Your button content here */}
-              </button>
-            </div>
-            {/* Rest of your code */}
-          </div>
-          <div className="mx-auto mt-12 flex max-w-[550px] items-center justify-end space-x-5">
-            <button
-              className="flex h-[70px] w-[70px] items-center justify-center rounded-full bg-[#6A64F1] text-white"
-              onClick={chatboxToggleHandler}
-            >
-              {/* Your button content here */}
-            </button>
-          </div>
+    return(
+        <div className='mt-2'>
+        <button onClick={onClick} className='bg-orange-500 font-bold py-2 px-4 rounded-md  text-white inline-block'>{props.children}</button>
         </div>
-      </div>
-    </div>
-  );
+    )
 }
 
-export default Contact;
+export default Button;
